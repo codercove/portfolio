309bb717e308515932aac8e53934b68b460d1b74
ncoderlk <ncoderlk@gmail.com>
2022-12-17 04:44:43 +0530

    updated

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer container">
+      <span className="footer-copy">
+        &copy; {new Date().getFullYear()} Sithika. Al rights reserved
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
